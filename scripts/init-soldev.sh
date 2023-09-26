5f9139a8a22e9799d84afcf261fbb45a10e3b5e8
hungngo <qhung.bm0409@gmail.com>
2023-09-26 11:32:42 +0700

    update

--- scripts/init-soldev.sh
+++ scripts/init-soldev.sh
@@ -16,6 +16,50 @@ function initHardHat() {
     pnpm install hardhat
     pnpm hardhat init
     mv -f contracts src
+    tsconfig='{
+        "compilerOptions": {
+            "target": "es2020",
+            "module": "commonjs",
+            "strict": false,
+            "esModuleInterop": true,
+            "outDir": "dist",
+            "forceConsistentCasingInFileNames": true
+        },
+        "include": ["./scripts/**/*", "./test/**/*"],
+        "files": ["hardhat.config.ts"]
+    }'
+    echo "$tsconfig" > tsconfig.json
+
+    hardhatconfig='
+            import { HardhatUserConfig } from "hardhat/config";
+            import "@nomicfoundation/hardhat-toolbox";
+
+            const config: HardhatUserConfig = {
+            paths: {
+                sources: "./src",
+                tests: "./test",
+                cache: "./cache",
+                artifacts: "./artifacts",
+            },
+            solidity: {
+                version: "0.8.19",
+                settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200,
+                },
+                },
+            },
+            typechain: {
+                outDir: "typechain",
+                target: "ethers-v5", // Use the ethers-v5 target
+            },
+            };
+
+            export default config;'
+    echo "$tsconfig" > hardhat.config.ts
+    cd ..
+    make lint
 }
 
 read -rp "Init you sol dev tool! Enter 1 for Forge or 2 for HardHat: " choice
